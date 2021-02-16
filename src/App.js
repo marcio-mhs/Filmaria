05ac254fc7ac4c6ef03ea08c33489acdf3f72338
marcio-mhs <marciomhs@hotmail.com>
2021-02-16 09:53:04 -0300

    aula 51

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import Routes from './routes';
+import './style.css'
 
 class App extends Component {
   render(){
-    return (
-      <div className="App">
+    return(
+      <div className="app">
+        <Routes/>
       </div>
     );
   }  
